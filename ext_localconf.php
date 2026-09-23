3296f03b11f57b5990c20c5f994f557623a37e8b
Martin Neumann <neumann@sitegeist.de>
2026-09-23 12:21:21 +0200

    v14 update

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,19 +2,11 @@
 
 defined('TYPO3') || die();
 
-// Register icon
-/** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-$iconRegistry->registerIcon(
-    'content-photographer',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:photographer/Resources/Public/Icons/Extension.svg']
-);
+// Icon registration now happens via Configuration/Icons.php (TYPO3 v14:
+// instantiating IconRegistry in ext_localconf.php is no longer allowed).
 
-// Auto-include PageTSConfig for New Content Element Wizard (so the CE appears without manual PageTS include)
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    "@import 'EXT:photographer/Configuration/TsConfig/Page/Mod/Wizards.tsconfig'"
-);
+// PageTSConfig for the New Content Element Wizard is auto-included via Configuration/page.tsconfig
+// (ExtensionManagementUtility::addPageTSConfig() was removed in TYPO3 v14).
 
 // Auto-include TypoScript setup (so rendering works without adding the static template manually)
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
